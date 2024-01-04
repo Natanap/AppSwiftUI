f96aed52ac7cf47e13aa2ffa3f17ff1a08be9761
m390853 <natanalvespr@gmail.com>
2024-01-04 20:53:32 -0300

    Initial Commit

---
+++ AppSwiftUI/AppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwiftUIApp.swift
+//  AppSwiftUI
+//
+//  Created by Natanael Alves Pereira on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AppSwiftUI
+//
+//  Created by Natanael Alves Pereira on 04/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
